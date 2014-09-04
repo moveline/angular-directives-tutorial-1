84bf1a7a173dfab5ead6156b6f0acfeaf4433684
adamgibbons <adam.d.gibbons@gmail.com>
2014-09-03 17:08:30 -0700

    corrects angular syntax errors

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
-var app = angular.module('App', []);
+var app = angular.module('app', []);
 
 app.controller('ContactsCtrl', function($scope) {
-  $scope.contacts = [
+  $scope.moveliners = [
     {name: 'Adam', title: 'Director, Thought Engineering'},
     {name: 'Brittany', title: 'Lead Whisperer, Cat Rescue League'},
-    {name: 'Russell', title: 'Owner, Javascript n\' drink of Brooklyn'}
+    {name: 'Russell', title: 'Owner, Javascript n\' Drink of Brooklyn'}
   ];
 });
 
-app.directive('mvlnContact', function() {
+app.directive('mvlnContacts', function() {
   return {
     restrict: 'EA',
-    template: '<div><p>{{contact.name}}</p><button ng-click="next()">Next</button></div>',
+    templateUrl: './moveliner.html',
     scope: {
-      contacts: '='
+      contacts: '=contacts'
     },
     link: function(scope, element, attributes) {
       scope.contact = scope.contacts[0];
@@ -27,33 +27,3 @@ app.directive('mvlnContact', function() {
     }
   }
 });
-
-
-// var app = angular.module("App", []);
-
-// app.controller("ContactsCtrl", function($scope) {
-//   $scope.moveliners = [
-//     {name: 'Adam', title: 'Director, Thought Engineering'},
-//     {name: 'Brittany', title: 'Lead Whisperer, Cat Rescue League'},
-//     {name: 'Russell', title: 'Owner, Javascript n\' drink of Brooklyn'}];
-// });
-
-// app.directive("mvlnContacts", function() {
-//   return {
-//     restrict: 'EA',
-//     scope: {
-//       contacts: '='
-//     },
-//     link: function(scope, el, attrs) {
-//       scope.contact = scope.contacts[0];
-//       scope.next = function() {
-//         if (scope.contact == scope.contacts[scope.contacts.length - 1]) {
-//           scope.contact = scope.contacts[0];
-//         } else {
-//           scope.contact = scope.contacts[scope.contacts.indexOf(scope.contact) + 1]
-//         }
-//       };
-//     },
-//     template: '<div><p>{{contact.name}}</p><button ng-click="next()">Next</button></div>'
-//   };
-// });
